da131e8b9b1acddc63ec6feb659c631be2c332c3
KriPlZ <KriPlZ@KriPlZs-MacBook-Pro.local>
2024-11-13 21:09:05 +0300

    added sh script, task info moved to README

--- network_communication.py
+++ network_communication.py
@@ -1,25 +1,3 @@
-'''
-программа должна включать один package в котором:
-    создан класс MySender и:
-        - конструктор, принимающий на вход реквизиты сервера для подключения
-        - protected метод который генерирует случайную последовательность байт размером до 8 ГБ, максимальный размер 
-        устанавливается константой
-        - public метод который создает tcp-соединение с переданным через конструктор сервером (в случае если соединение 
-        установить не получилось из-за недоступности сервера, делается еще 2 попытки с интервалом 10с) и 
-        отправляет случайную последовательность байт по tcp в формате <размер><данные>, при этом <размер> должен занимать 
-        минимальное необходимое количество байт и корректно декодироваться на принимающей стороне
-    
-    создан класс MyListener и:
-        - конструктор, принимающий на вход реквизиты интерфейса на котором должен работать сервис
-        - public метод который запускает tcp-сервис на переданных через конструктор ip-адресе и порту
-        - protected метод который получает последовательность байт по tcp в формате <размер><данные> декодирует их 
-        и выводит в stdout размер в виде целого числа и первые 16 байт (не более 16 байт) в hex-формате блока данных
-
-    в функции main 
-        - для роли отправителя с использованием класса MySender выполняется одна отправка
-        - для роли принимающей стороны с использованием класса MyListener поднимается tcp-сервис и 
-        ожидает/обрабатывает подключениядля приложения 
-'''
 import random
 import os
 import socket
@@ -27,7 +5,7 @@ import time
 import sys
 import argparse
 
-#MINPORT = 60001
+MINPORT = 60000
 
 class MySender:
     
@@ -66,6 +44,7 @@ class MySender:
             
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                     print(f'Попытка подключения {i+1} к серверу {self.ip}:{port}')
                     s.connect((self.ip, port))
                     data_to_send = self._generate_random_bytes()
@@ -101,6 +80,7 @@ class MyListener:
         
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((self.ip, self.port))
             s.listen()
             print(f'Сервис запущен на {self.ip}:{self.port}')
@@ -123,7 +103,8 @@ class MyListener:
                     self._decode_data(data)
                         
 def main():
-    MINPORT = 60000
+    global MINPORT
+    
     parser = argparse.ArgumentParser(description='Программа для отправки и приема данных')
     #обязательный аргумент, который определяет роль приложения
     parser.add_argument('--role', choices=['send', 'recieve'], required=True, help='Роль приложения: send или recieve')
@@ -141,8 +122,8 @@ def main():
             print(f'Не указан адрес для отправки')
             sys.exit(1)
         sender = MySender(args.server)
-        sender.connect(args.port)
-    #    MINPORT+=1
+        sender.connect(args.port if args.port else MINPORT)
+        MINPORT += 1
         
     elif args.role == 'recieve':
         print(f'Создаем приложение для приема данных')
